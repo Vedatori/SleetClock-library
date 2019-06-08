ac01ad920aca547497d389a9940614fc776b4f49
Petr Vávra <xvavra19@sstud.feec.vutbr.cz>
2019-06-08 17:16:58 +0200

    DarkSkyParser

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
 #include <Arduino.h>
 #include <WiFiManager.h>
 #include "SleetClock.h"
+#include "JsonListener.h"
+#include "DarkskyParser.h"
+
 
 SleetClock sleetClock;
 Adafruit_PCD8544 display = Adafruit_PCD8544(23, 19, 18, 5, 32);
@@ -24,6 +27,102 @@ void saveParamCallback(){
   Serial.println("PARAM darkSkyApiKey = " + getParam("darkSkyKeyId"));
   Serial.println("PARAM coordinatesLatitude = " + getParam("coordinateLatitude"));
   Serial.println("PARAM coordinatesLongtitude = " + getParam("coordinateLongtitude"));
+}
+
+DarkskyParser dsParser;
+
+const char *apiKey = "646f7e8e4fb6b4a169d193a8cc67ee2f";
+const char *latitude = "49.2002";
+const char *longitude = "16.6078";
+
+const char *tz = "0";
+const char *ntpServer1 = "0.pool.ntp.org";
+const char *ntpServer2 = "1.pool.ntp.org";
+const char *ntpServer3 = "2.pool.ntp.org";
+
+/* weather definition */
+enum {
+  CLEAR_DAY = 0, CLEAR_NIGHT = 1, CLOUDY = 2,
+  RAIN = 3, HEAVY_RAIN = 4,
+  SNOW = 5, SLEET = 6, WIND = 7, FOG = 8,
+  PARTLY_CLOUDY_DAY = 9, PARTLY_CLOUDY_NIGHT = 10,
+  UNAVAILABLE = 11, OTHER = 12, INITIAL = 13,
+};
+
+void printInfo(void *arg) {
+  int currentWeather[12], lastWeather[12];
+  int currentPrecipProbability[12], lastPrecipProbability[12];
+  struct tm timeInfo;
+  time_t lastUpdate = 0;
+  int lastHour = -1;
+  float lastTemperature = 0, lastHumidity = 0;
+  char s[9];
+
+  while (1) {
+    time_t currentTime = time(NULL);
+
+    /* check if the data is valid */
+    if ((dsParser.lastUpdate == 0) || ((currentTime - dsParser.lastUpdate) > 3600)) {
+      /* no data or data is too old */
+      for (int i = 0; i < 12; i++) {
+        currentWeather[i] = UNAVAILABLE;
+      }
+    } else if (lastUpdate < dsParser.lastUpdate) {
+      /* dark sky data has been updated */
+      for (int i = 0, pos = dsParser.currentHour; i < 13; i++) {
+        if (i == 1) { /* skip dsParser.weatherData[1]. it is forecast data of current hour so abandon it. */
+          continue;
+        }
+        /* set weather and precipitation probablity */
+        
+        currentPrecipProbability[pos % 12] = dsParser.weatherData[i].precipProbability;
+        pos++;
+      }
+      lastUpdate = dsParser.lastUpdate;
+    }
+
+    /* draw weather icon and precipProbability */
+    for (int i = 0; i < 12; i++) {
+      if (currentWeather[i] != lastWeather[i]) {
+        lastWeather[i] = currentWeather[i];
+      }
+      if (currentPrecipProbability[i] != lastPrecipProbability[i]) {
+        if (currentWeather[i] < UNAVAILABLE) {
+          lastPrecipProbability[i] = currentPrecipProbability[i];
+        }
+      }
+    }
+
+    /* write temperature */
+    if (lastTemperature != dsParser.weatherData[0].temperature) {
+      sprintf(s, "%5.1fC", dsParser.weatherData[0].temperature);
+      
+      Serial.println(s); 
+      lastTemperature = dsParser.weatherData[0].temperature;
+    }
+
+    getLocalTime(&timeInfo);
+    sprintf(s, "%02d:%02d:%02d", timeInfo.tm_hour, timeInfo.tm_min, timeInfo.tm_sec);
+    Serial.println(s);
+
+    /* hour changed. update the last hour data. delete underline at previous hour */
+    if (lastHour != timeInfo.tm_hour) {
+      int t = (timeInfo.tm_hour + 11) % 12;
+        
+      currentPrecipProbability[t] = dsParser.weatherData[13].precipProbability;
+      lastHour = timeInfo.tm_hour;
+    }
+    
+    
+    /* draw humidity */
+    if (lastHumidity != dsParser.weatherData[0].humidity) {
+      sprintf(s, "%5.1f%%", dsParser.weatherData[0].humidity);    
+       Serial.println(s);
+      lastHumidity = dsParser.weatherData[0].humidity;
+    }
+
+    delay(100);
+  }
 }
 
 void setup() {
@@ -65,6 +164,14 @@ void setup() {
     Serial.println("Connected to wi-fi");
   }
   
+  Serial.println("Powered by DarkSky");
+  Serial.println("Powered by DarkSky");
+
+  xTaskCreatePinnedToCore(printInfo, "printInfo", 2048, NULL, 1, NULL, 0);
+  dsParser.begin(apiKey, latitude, longitude);
+  configTzTime(tz, ntpServer1, ntpServer2, ntpServer3);
+  dsParser.getData();
+
 }
 
 void loop() {
